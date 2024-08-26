f45fb55ba2bc32e586b5c00edf9a1ffa9c312485
Christopher Ralph <chris.734c@gmail.com>
2024-08-26 00:26:23 -0400

    👍 IMPROVE: Removed the project_id from the get tasks response via the task model to remove redundant info

--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,27 @@
 const db = require('../../data/dbConfig');
 // build your `Task` model here
 
-function findTasks() {
-  return db('tasks as tk').leftJoin(
-    'projects as pj',
-    'tk.project_id',
-    'pj.project_id'
-  ).select('tk.*', 'pj.project_name', 'pj.project_description')
+async function findTasks() {
+  const tasks = await db('tasks as tk')
+    .leftJoin('projects as pj', 'tk.project_id', 'pj.project_id')
+    .select('tk.*', 'pj.project_name', 'pj.project_description');
+
+  return tasks.map((task) => {
+    const { project_id, ...taskWithoutProjectId } = task; // eslint-disable-line
+    return {
+      ...taskWithoutProjectId,
+      task_completed: Boolean(task.task_completed),
+    }
+  });
 }
 
-function findTaskById(task_id) {
-  return db('tasks').where('task_id', task_id).first();
+async function findTaskById(task_id) {
+  const specificTask = await db('tasks').where('task_id', task_id).first();
+
+  return {
+    ...specificTask,
+    task_completed: Boolean(specificTask.task_completed),
+  };
 }
 
 async function addTask(task) {
