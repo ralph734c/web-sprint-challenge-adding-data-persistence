96dcec00ca20daeeee901d36acd96fe0f7c45ede
Christopher Ralph <chris.734c@gmail.com>
2024-08-25 22:41:02 -0400

    📦 NEW: Scaffolded basics around routers for projects, resources, and tasks. Updated the server.

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,31 @@
 // build your `/api/resources` router here
+const resourcesModel = require('../resource/model');
+const router = require('express').Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    res.json({
+      message: `The ${req.method} to ${req.originalUrl} is working!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    res.json({
+      message: `The ${req.method} to ${req.originalUrl} is working!`,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.use((err, req, res, next) => { // eslint-disable-line
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 // build your server here and require it from index.js
 const express = require('express');
+const projectsRouter = require('./project/router');
+const resourcesRouter = require('./resource/router');
+const tasksRouter = require('./task/router');
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'The API is running' });
